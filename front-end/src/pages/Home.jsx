9e7b7ce4fb5d9fcea9e729af7f1d588f20b88d33
enzoranelli <enzo.ranelli@gmail.com>
2024-09-11 10:48:00 -0300

    Termine login, realice un protipo de agregar usuario y objetivo

--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -1,10 +1,64 @@
 import { usePageTitle } from "../components/nombrePag";
+import { useState } from "react";
 function Home(){
     usePageTitle('Home | Medicion de objetos')
-    return(
-        <>
-        <h1>Home</h1>
-        </>
+    const [texto, setTexto] = useState("");
+    const [opcionSeleccionada, setOpcionSeleccionada] = useState("");
+    const [puntuacion, setPuntuacion] = useState(0);
+    // Función para manejar el envío del formulario
+    const manejarEnvio = (e) => {
+        e.preventDefault();
+        // Aquí puedes procesar los valores del formulario
+        console.log("Texto:", texto);
+        console.log("Opción seleccionada:", opcionSeleccionada);
+    };
+
+    return (
+        <div className="objetivo-container">
+        <h1>Agregar objetivo</h1>
+        <form onSubmit={manejarEnvio}>
+        <div>
+            <label htmlFor="texto">Descripcion del objetivo</label>
+            <textarea
+            type="text"
+            id="texto"
+            value={texto}
+            onChange={(e) => setTexto(e.target.value)}
+            rows="7"
+            cols="50"
+            />
+        </div>
+
+        <div>
+            <h3>Asigne el objetivo a un area </h3>
+            <label htmlFor="opcion">Selecciona una opción:</label>
+            <select
+            id="opcion"
+            value={opcionSeleccionada}
+            onChange={(e) => setOpcionSeleccionada(e.target.value)}
+            >
+            <option value="">--Selecciona una opción--</option>
+            <option value="opcion1">Marketing</option>
+            <option value="opcion2">Operaciones</option>
+            <option value="opcion3">Contabilidad</option>
+            </select>
+        </div>
+        <div>
+            <label htmlFor="slider">Asigne porcentaje de puntuación </label>
+            <input 
+                type="range"
+                id="puntuacion"
+                min="0"
+                max="100"
+                value={puntuacion}
+                onChange={(e)=>setPuntuacion(e.target.value)}
+            />
+            <label>{puntuacion}%</label>
+        </div>
+        <button type="submit">Enviar</button>
+        </form>
+        </div>
+        
     );
 
 }
--- front-end/src/pages/NuevoUsuario.jsx
+++ front-end/src/pages/NuevoUsuario.jsx
@@ -1,8 +1,118 @@
+import React from "react";
+import {useForm} from 'react-hook-form'
+import '../styles/FormUsuario.css'
+
 function NuevoUsuario(){
+  const {register, handleSubmit} = useForm();
+  
+  const onSubmit = (data) =>{
+    
+    const verificar = verificarFormulario(data);
+    
+    console.log(verificar)
+    if(verificar){
+        
+      alert('Empleado agregado correctamente!!');
+      console.log(data)
+
+    }else{
+        alert('Campos incompletos');
+    }
+    
+}
+
+  function verificarFormulario(data){
+    console.log(data)
+    for(const propiedad in data){
+        if(data.hasOwnProperty(propiedad)){
+            const valor = data[propiedad];
+            if(valor === '' || valor === ""){
+                console.log('Esto dio falso')
+                return false;
+            }
+        }
+    }
+    data.rol = data.rol=== 'admin'
+    console.log(data.usuarioPassword);
+    console.log(data.confirmar);
+    return data.usuarioPassword === data.confirmar ? true : false;
+    
+}
     return (
-        <>
-            <h1>Crear nuevo Usuario</h1>
-        </>
+      <div className="form-container">
+        
+
+            <form className="form-agregar" onSubmit={handleSubmit(onSubmit)}>
+                <h1 className="titulo">Agregar nuevo usuario</h1>
+                <div className="contenedor-input">
+                    <label className="label-form">Nombre</label>
+                    <input type="text" className="input-text" {...register('nombre')}></input>
+                </div>
+
+                <div className="contenedor-input">
+                    <label>Apellido</label>
+                    <input type="text" className="input-text" {...register('apellido')}></input>
+                </div>
+
+                <div className="contenedor-input">
+                    <label>Puesto</label>
+                    <input type="text" className="input-text" {...register('puesto')}></input>
+                </div>
+
+                <div className="contenedor-input">
+                    <label>Area</label>
+                    <input type="text" className="input-text" {...register('area')}></input>
+                </div>
+
+              
+                
+
+                <div className="contenedor-input">
+                    <label>Correo electronico<b>**</b></label>
+                    <input type="email" className="input-text" {...register('correo',{autoComplete:"off"})}></input>
+                </div>
+
+                <div className="contenedor-input">
+                    <label>Contraseña</label>
+                    <input type="password" className="input-text" {...register('usuarioPassword',{autoComplete:"off"})}></input>
+                </div>
+
+                <div className="contenedor-input">
+                    <label>Confirmar contraseña</label>
+                    <input type="password" className="input-text" {...register('confirmar')}></input>
+                </div>
+
+                <div className="contenedor-input">
+                    <label><b>¿Es administrador?</b></label>
+                    <div className="contenedor-radio">
+                        <label className="label-radio">
+                            <input
+                                className="input-radio"
+                                type="radio"
+                                value="admin"
+                                {...register('rol')}
+                            />
+                            Sí
+                        </label>
+
+                        <label className="label-radio">
+                            <input
+                                className="input-radio"
+                                type="radio"
+                                value="user"
+                                {...register('rol')}
+                            />
+                            No
+                        </label>
+                    </div>
+                </div>
+                <div className="contenedor-input">
+                    <p className="aviso">{'(**) Los campos marcados tienen que ser unicos para cada empleado.'}</p>
+                    <input type="submit" value="Agregar" className="boton-submit"/>
+                </div>
+                
+            </form>
+      </div>
     );
 }
 export default NuevoUsuario;
